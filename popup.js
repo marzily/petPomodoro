6f4c99d290c7e2be155f310ce8513f437a81973d
margie <ly.margarett@gmail.com>
2019-08-28 21:55:25 -0700

    add code to make post request to petfinder

--- popup.js
+++ popup.js
@@ -1,15 +1,24 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
+//
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+//
+// changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.executeScript(
+//           tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+//   };
+let catButton = document.getElementById("cat");
+let dogButton = document.getElementById("dog");
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://api.petfinder.com/v2/oauth2/token", true);
+// xhttp.setRequestHeader("grant_type", "");
+xhttp.send("grant_type=client_credentials&client_id=" + keys.petFinderKey + "&client_secret=" + keys.petFinderSecret);
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
+console.log(xhttp.responseText);
