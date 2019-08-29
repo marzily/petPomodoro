17ea05fcb73b2dd3b1f93dbba1e730c2b54f4a46
margie <ly.margarett@gmail.com>
2019-08-28 22:40:55 -0700

    create css file

--- popup.js
+++ popup.js
@@ -16,9 +16,9 @@
 let catButton = document.getElementById("cat");
 let dogButton = document.getElementById("dog");
 
-xhttp = new XMLHttpRequest();
-xhttp.open("GET", "https://api.petfinder.com/v2/oauth2/token", true);
-// xhttp.setRequestHeader("grant_type", "");
-xhttp.send("grant_type=client_credentials&client_id=" + keys.petFinderKey + "&client_secret=" + keys.petFinderSecret);
-
-console.log(xhttp.responseText);
+// xhttp = new XMLHttpRequest();
+// xhttp.open("GET", "https://api.petfinder.com/v2/oauth2/token", true);
+// // xhttp.setRequestHeader("grant_type", "");
+// xhttp.send("grant_type=client_credentials&client_id=" + keys.petFinderKey + "&client_secret=" + keys.petFinderSecret);
+//
+// console.log(xhttp.responseText);
